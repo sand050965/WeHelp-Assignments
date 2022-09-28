71c81b389f22bebb9b3e0bdddbe40b91899f8fac
sand050965 <sand050965@gmail.com>
2022-09-28 17:34:07 +0800

    Add Time Complexity Notation

--- week-2/Python.py
+++ week-2/Python.py
@@ -1,3 +1,4 @@
+# O(n)
 # 要求一: 函式與流程控制
 # 完成以下函式，在函式中使用迴圈計算最小值到最大值之間，固定間隔的整數總和。其中你可以假設 max 一定大於 min 且為整數，step 為正整數。
 def calculate(min, max, step):
@@ -17,6 +18,7 @@ calculate(-1, 2, 2)  # 你的程式要能夠計算 -1+1，最後印出 0
 
 print("*****************")
 
+# O(n)
 # 要求二: Python 字典與列表
 # 完成以下函式，正確計算出非 manager 的員工平均薪資，所謂非 manager 就是在資料中 manager 欄位標註為 False (Python) 或 false (JavaScript) 的員工，程式需考慮員工資料數量 不定的情況。
 def avg(data):
@@ -55,6 +57,7 @@ avg({
 
 print("*****************")
 
+# O(n)
 # 要求三:
 # 完成以下函式，最後能印出程式中註解所描述的結果。
 def func(a):
@@ -71,6 +74,7 @@ func(-3)(2, 9)  # 你補完的函式能印出 -3+(2*9) 的結果 15
 
 print("*****************")
 
+# O(n^2)
 # 要求四:
 # 找出至少包含兩筆整數的列表 (Python) 或陣列 (JavaScript) 中，兩兩數字相乘後的最大值。
 def maxProduct(nums):
@@ -88,15 +92,16 @@ def maxProduct(nums):
 
 
 maxProduct([5, 20, 2, 6])  # 得到 120
-# maxProduct([10, -20, 0, 3])  # 得到 30
-# maxProduct([10, -20, 0, -3])  # 得到 60
-# maxProduct([-1, 2])  # 得到 -2
-# maxProduct([-1, 0, 2])  # 得到 0
-# maxProduct([5, -1, -2, 0])  # 得到 2
-# maxProduct([-5, -2])  # 得到 10
+maxProduct([10, -20, 0, 3])  # 得到 30
+maxProduct([10, -20, 0, -3])  # 得到 60
+maxProduct([-1, 2])  # 得到 -2
+maxProduct([-1, 0, 2])  # 得到 0
+maxProduct([5, -1, -2, 0])  # 得到 2
+maxProduct([-5, -2])  # 得到 10
 
 print("*****************")
 
+# O(n)
 # 要求五:
 # Given an array of integers, show indices of the two numbers such that they add up to a specific target. You can assume that each input would have exactly one solution, and you can not use the same element twice.
 def twoSum(nums, target):
@@ -114,6 +119,7 @@ print(result)  # show [0, 2] because nums[0]+nums[2] is 9
 
 print("*****************")
 
+# O(n)
 # 要求六:
 # 給定只會包含 0 或 1 兩種數字的列表 (Python) 或陣列 (JavaScript)，計算連續出現 0 的最大長度。
 def maxZeros(nums):
