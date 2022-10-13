2d2298e263d6092db99fe427ec8e01fd130d0d13
sand050965 <sand050965@gmail.com>
2022-10-13 11:11:30 +0800

    feat: Alter line in app.py

--- week-4/app.py
+++ week-4/app.py
@@ -1,8 +1,9 @@
 from flask import Flask  # 載入flask
 from flask import request  # 載入 Request 物件
+from flask import Response  # 載入 Response 物件
+from flask import make_response # 載入 make_response 物件
 from flask import redirect  # 載入 redirect 物件
 from flask import render_template  # 載入 render_template 物件
-from flask import session  # 載入 session 物件
 
 #  建立 application 物件
 app = Flask(__name__)
@@ -12,7 +13,7 @@ app.secret_key = "secret_key"
 # 處理路徑 / 對應的處理函式
 @app.route("/")
 def index():
-    if ("username" in session):
+    if ("username" in request.cookies):
         return redirect("/member")
     return render_template("index.html")
 
@@ -27,14 +28,13 @@ def doSignIn():
     str = "test"
     blankStr = ""
     if (acct == str and pwd == str):
-        session["username"] = acct  # session["欄位名稱"] = 資料
-        return redirect("/member")  # 導向到路徑 /member
+        response = make_response(redirect("/member"))  # 導向到路徑 /member
+        response.set_cookie("username", acct)  # 將資料存入 cookie
+        return response
     elif (acct.strip() == blankStr or pwd.strip() == blankStr):
-        session.pop("username", None)
         msg = "請輸入帳號、密碼"
         return redirect("/error?message=" + msg)
     elif (acct != str or pwd != str):
-        session.pop("username", None)
         msg = "帳號、或密碼輸入錯誤"
         return redirect("/error?message=" + msg)
 
@@ -42,7 +42,7 @@ def doSignIn():
 # 處理路徑 /member 對應的處理函式
 @ app.route("/member")
 def doMember():
-    if ("username" in session):
+    if ("username" in request.cookies):
         return render_template("member.html")
     return redirect("/")
 
@@ -50,9 +50,10 @@ def doMember():
 # 使用 GET 方法，處理路徑 /signout 對應的處理函式
 @app.route("/signout", methods=["GET"])
 def doSignOut():
-    # remove the username from the session if it's there
-    session.pop("username", None)
-    return redirect("/")
+    response = make_response(redirect("/"))
+    # remove the username from the cookie if it's there
+    response.delete_cookie("username", None)
+    return response
 
 # 處理路徑 /error 對應的處理函式
 @ app.route("/error")
