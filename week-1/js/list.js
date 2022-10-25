c3ed1e274ce165e0e68476abe95ddb2e39990a77
sand050965 <sand050965@gmail.com>
2022-10-25 17:10:58 +0800

    Style: Modify formatting in week-1, week-3, week-4 files

--- week-1/js/list.js
+++ week-1/js/list.js
@@ -1,13 +1,13 @@
 let sideMenu = document.getElementById("side-menu");
 
 function openSideMenu() {
-    let event = window.event;
-    sideMenu.style.width = "250px";
-    event.stopPropagation();
+  let event = window.event;
+  sideMenu.style.width = "250px";
+  event.stopPropagation();
 }
 
 function closeSideMenu() {
-    if (sideMenu.style.width  == "250px") {
-        sideMenu.style.width  = "0px";    
-    }
+  if (sideMenu.style.width == "250px") {
+    sideMenu.style.width = "0px";
+  }
 }
